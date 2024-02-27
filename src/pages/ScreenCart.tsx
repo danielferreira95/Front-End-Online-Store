b79933db4c01935b8ecb7a6553c80609a6bbede2
larissa recco <larissarsantos@uol.com.br>
2024-02-27 17:15:24 -0300

    feat:create shopping cart page

---
+++ src/pages/ScreenCart.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ScreenCart() {
+  return (
+    <div>
+      <h1>Carrinho de Compras</h1>
+      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+    </div>
+  );
+}
+
+export default ScreenCart;
